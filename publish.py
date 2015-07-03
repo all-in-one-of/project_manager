709a70d519e6274a53c7e71d9c7e1a3945651253
ThibH <houdon.thibault@gmail.com>
2015-07-03 13:39:05 -0400

    Added a publish script for publishing manager on Nature network HDD

---
+++ publish.py
@@ -0,0 +1,12 @@
+import distutils.core
+
+distutils.dir_util.copy_tree("H:\\01-NAD\\_pipeline\\_utilities\\_asset_manager\\ui", "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\_pipeline\\_utilities\\_asset_manager\\ui")
+
+distutils.dir_util.copy_tree("H:\\01-NAD\\_pipeline\\_utilities\\_asset_manager\\media", "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\_pipeline\\_utilities\\_asset_manager\\media")
+
+distutils.dir_util.copy_tree("H:\\01-NAD\\_pipeline\\_utilities\\_asset_manager\\lib", "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\_pipeline\\_utilities\\_asset_manager\\lib")
+
+distutils.dir_util.copy_tree("H:\\01-NAD\\_pipeline\\_utilities\\_asset_manager\\bin", "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\_pipeline\\_utilities\\_asset_manager\\bin")
+
+distutils.file_util.copy_file("H:\\01-NAD\\_pipeline\\_utilities\\_asset_manager\\app.py", "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\_pipeline\\_utilities\\_asset_manager\\app.py")
+
